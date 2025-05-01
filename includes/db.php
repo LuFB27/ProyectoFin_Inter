f61655539b1864b0deaac9a22640f5c5ce3b5c87
Lu <lucia.fernandez.barrio.113@gmail.com>
2025-04-30 23:17:36 -0600

    Agregar cuenta e inicio de sesion, tambien cambios de css

---
+++ includes/db.php
@@ -0,0 +1,41 @@
+
+<?php
+ 
+
+$host = "localhost";
+ 
+
+$user = "root";
+ 
+
+$pass = ""; // si no cambiaste la contraseña de root en Laragon
+ 
+
+$db = "tienda_peluches";
+ 
+
+
+ 
+
+// Crear conexión
+ 
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+ 
+
+
+ 
+
+// Verificar conexión
+ 
+
+if (!$conn) {
+ 
+
+  die("Conexión fallida: " . mysqli_connect_error());
+ 
+
+}
+ 
+
+?>
